2853e306c142ad9c9e0850de8f3d2dda9dde7fcf
Hengjian Jia <henryjia18@gmail.com>
2023-04-24 15:17:56 +0100

    Add some example files for testing on avon

--- BBBP/mdsim/run.py
+++ BBBP/mdsim/run.py
@@ -1,3 +1,7 @@
+# Example:
+# python run.py --smiles "CCC(=O)N(C1CCN(CC1)CCc2ccccc2)c3ccccc3" --mol_name 'main' --working_dir 'fentanyl/' --solvate
+# python run.py --smiles "CCC(=O)N(C1CCN(CC1)CCc2ccccc2)c3ccccc3" --mol_name 'main' --working_dir 'fentanyl/' --gen_mdp --em_steep --em_lbfgs --npt --nvt --md --lam 0 --ntomp 8 --ntmpi 1
+
 import argparse
 import os
 import shutil
