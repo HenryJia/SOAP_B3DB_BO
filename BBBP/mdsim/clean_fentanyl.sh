2932c1dad1e1a2cfc3b0f4e8714cfc7abe0bc9a4
Hengjian Jia <henryjia18@gmail.com>
2023-07-13 14:30:02 +0100

    Stop tracking the BBBP mdsim files

--- BBBP/mdsim/clean_fentanyl.sh
+++
@@ -1,2 +0,0 @@
-mv fentanyl/input_files/\#main.top.1\# fentanyl/input_files/main.top
-rm -rf fentanyl/output_files fentanyl/input_files/\#main.top.*
--- BBBP/mdsim/mdsim_example.sh
+++
@@ -1,4 +0,0 @@
-python -m pipenv shell
-
-#python run.py --smiles "CCC(=O)N(C1CCN(CC1)CCc2ccccc2)c3ccccc3" --mol_name 'main' --working_dir 'fentanyl/' --solvate
-python run.py --smiles "CCC(=O)N(C1CCN(CC1)CCc2ccccc2)c3ccccc3" --mol_name 'main' --working_dir 'fentanyl/' --gen_mdp --em_steep --em_lbfgs --npt --nvt --md --lam 0 --ntomp 48 --ntmpi 1 &> mdsim_example.log
